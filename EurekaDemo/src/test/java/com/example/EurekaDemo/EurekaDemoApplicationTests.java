1f0db58375e899f66355460dc7ec2bf7634d9048
Sulochana <sulochana2539@gmail.com>
2025-12-07 16:56:06 +0530

    Add files via upload

---
+++ EurekaDemo/src/test/java/com/example/EurekaDemo/EurekaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EurekaDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
